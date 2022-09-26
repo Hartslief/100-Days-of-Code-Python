950a7476551da68d4c5712c20493b899fe4f2f14
Caleb Hartslief <93492844+Hartslief@users.noreply.github.com>
2022-09-26 19:19:13 +0200

    Add files via upload

---
+++ Project Day #1.py
@@ -0,0 +1,8 @@
+#100 Days of Code Project: Day 1
+
+#Asking user for ther information
+CityNameInput = input("Please enter the city that you live in: ")
+AnimalNameInput = input("Please enter the name of your animal: ")
+
+#Printing the users information and combining it
+print("Your Band Name should be: ", CityNameInput + AnimalNameInput)
